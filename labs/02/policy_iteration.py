74b696adb61068a313c2a1542f1b3b547b84ed79
Filip Bartek <filip.bartek@hotmail.com>
2019-10-25 13:48:14 +0200

    Reformat line spacing automatically by PyCharm

--- labs/02/policy_iteration.py
+++ labs/02/policy_iteration.py
@@ -29,10 +29,11 @@ class GridWorld:
         offset_x = -1 if action == 3 else action == 1
         offset_y = -1 if action == 0 else action == 2
         new_x, new_y = x + offset_x, y + offset_y
-        if not(new_x >= 4 or new_x < 0  or new_y >= 3 or new_y < 0 or (new_x == 1 and new_y == 1)):
+        if not (new_x >= 4 or new_x < 0 or new_y >= 3 or new_y < 0 or (new_x == 1 and new_y == 1)):
             state = new_x + 4 * new_y
         if state >= 5: state -= 1
         return [probability, +1 if state == 3 else -100 if state == 6 else 0, state]
+
 
 def action_value_function(value_function, s, a, gamma):
     """
@@ -51,6 +52,7 @@ def action_value_function(value_function, s, a, gamma):
         result += probability * (reward + gamma * value_function[new_state])
     return result
 
+
 # Iterate to implement iterative policy evaluation.
 def improve_value_function_once(value_function, policy, gamma):
     output_value_function = [0] * GridWorld.states
@@ -58,6 +60,7 @@ def improve_value_function_once(value_function, policy, gamma):
         # The policy is deterministic: it always chooses action `policy[s]` in state `s`.
         output_value_function[s] = action_value_function(value_function, s, policy[s], gamma)
     return output_value_function
+
 
 def iterative_policy_evaluation(policy, iterations, gamma, value_function=None):
     """
@@ -80,6 +83,7 @@ def iterative_policy_evaluation(policy, iterations, gamma, value_function=None):
         logging.info(f'Iterative policy evaluation converged after {iteration_converged} iterations.')
     return value_function
 
+
 def greedy_policy(value_function, gamma):
     """
     Greedily choose a new policy based on a value function.
@@ -99,6 +103,7 @@ def greedy_policy(value_function, gamma):
                 best_a = a
         output_policy[s] = best_a
     return output_policy
+
 
 def policy_iteration(steps, iterations, gamma):
     """
@@ -124,6 +129,7 @@ if __name__ == "__main__":
 
     # Parse arguments
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--steps", default=10, type=int, help="Number of policy evaluation/improvements to perform.")
     parser.add_argument("--iterations", default=1, type=int, help="Number of iterations in policy evaluation step.")

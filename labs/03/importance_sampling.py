f13a7aefa4871eb9872bcdfb28cd1143a0193646
Filip Bartek <filip.bartek@hotmail.com>
2019-11-01 16:00:34 +0100

    Solve importance_sampling
    https://recodex.mff.cuni.cz/app/assignment/a5db4203-f38b-11e9-9ce9-00505601122b/solution/32039a61-fcb8-11e9-9ce9-00505601122b

--- labs/03/importance_sampling.py
+++ labs/03/importance_sampling.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
-import numpy as np
 import gym
+import numpy as np
+
+
+def pi(action):
+    result = 0
+    if action in [1, 2]:
+        result = 1 / 2
+    return result
+
+
+def b(actions):
+    return 1 / actions
+
 
 if __name__ == "__main__":
     # Fix random seed
@@ -8,9 +20,12 @@ if __name__ == "__main__":
 
     # Parse arguments
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
     args = parser.parse_args()
+
+    gamma = 1.0
 
     # Create the environment
     env = gym.make("FrozenLake-v0")
@@ -20,8 +35,14 @@ if __name__ == "__main__":
 
     # Behaviour policy is uniformly random.
     # Target policy uniformly chooses either action 1 or 2.
-    V = np.zeros(states)
+    V = np.zeros(states)  # Value function of target policy (choosing 1 or 2 uniformly)
     C = np.zeros(states)
+
+    try:
+        import tqdm
+        t = tqdm.tqdm(total=args.episodes, unit="episode")
+    except ModuleNotFoundError:
+        pass
 
     for _ in range(args.episodes):
         state, done = env.reset(), False
@@ -29,12 +50,29 @@ if __name__ == "__main__":
         # Generate episode
         episode = []
         while not done:
-            action = np.random.choice(actions)
+            action = np.random.choice(actions)  # Behaviour policy: choose any action uniformly.
             next_state, reward, done, _ = env.step(action)
             episode.append((state, action, reward))
             state = next_state
 
-        # TODO: Update V using weighted importance sampling.
+        # Update V using weighted importance sampling.
+        # See npfl122-03.pdf:30
+        g = 0
+        w = 1  # rho
+        for state, action, reward in reversed(episode):
+            w *= pi(action) / b(actions)
+            if w == 0:
+                break
+            g = gamma * g + reward
+            C[state] += w
+            V[state] += w / C[state] * (g - V[state])
+
+        if 't' in locals():
+            t.set_postfix({"value of state 0": V[0]})
+            t.update()
+
+    if 't' in locals():
+        t.close()
 
     # Print the final value function V
     for row in V.reshape(4, 4):

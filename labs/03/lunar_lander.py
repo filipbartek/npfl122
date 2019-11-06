3a366cf8ffa352432d94dd1b53d7161a8cbc9cf7
Filip Bartek <filip.bartek@hotmail.com>
2019-11-06 19:09:53 +0100

    lunar_lander: Implement a baseline Q-learning based solution

--- labs/03/lunar_lander.py
+++ labs/03/lunar_lander.py
@@ -3,11 +3,16 @@
 # Team members:
 # [Filip Bartek](https://recodex.mff.cuni.cz/app/user/9d1ef2af-eb87-11e9-9ce9-00505601122b)
 
+import logging
+
 import numpy as np
 
 import lunar_lander_evaluator
+import rl
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     # Fix random seed
     np.random.seed(42)
 
@@ -17,31 +22,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--episodes", default=None, type=int, help="Training episodes.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
-    parser.add_argument("--alpha", default=None, type=float, help="Learning rate.")
-    parser.add_argument("--alpha_final", default=None, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=None, type=float, help="Exploration factor.")
-    parser.add_argument("--epsilon_final", default=None, type=float, help="Final exploration factor.")
-    parser.add_argument("--gamma", default=None, type=float, help="Discounting factor.")
+    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
+    parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor.")
+    parser.add_argument("--gamma", default=1.0, type=float, help="Discounting factor.")
+    parser.add_argument("--stats_plot_each", type=int)
     args = parser.parse_args()
 
     # Create the environment
     env = lunar_lander_evaluator.environment()
 
-    # The environment has env.states states and env.actions actions.
+    logging.info('Beginning training.')
+    q, _ = rl.perform(env, train=True, evaluate=False, episodes=args.episodes, epsilon=args.epsilon, alpha=args.alpha,
+                      gamma=args.gamma, render_each=args.render_each, stats_plot_each=args.stats_plot_each)
 
-    # TODO: Implement a suitable RL algorithm.
-    #
-    # The overall structure of the code follows.
-    while args.episodes is None or env.episode < args.episodes:
-        # To generate expert trajectory, you can use
-        state, trajectory = env.expert_trajectory()
-
-        # Perform a training episode
-        state, done = env.reset(), False
-        while not done:
-            if args.render_each and env.episode and env.episode % args.render_each == 0:
-                env.render()
-            action = 0
-            next_state, reward, done, _ = env.step(action)
-
-    # Perform last 100 evaluation episodes
+    logging.info('Beginning evaluation.')
+    rl.perform(env, train=False, evaluate=True, episodes=100, gamma=args.gamma, render_each=args.render_each)

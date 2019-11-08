2258cebafaaeb805ab8ffdf8ac7aac9d4c22e3af
Filip Bartek <filip.bartek@hotmail.com>
2019-11-08 11:07:30 +0100

    Add a special Bash script for running lunar_lander in Slurm

---
+++ labs/03/lunar_lander.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=60
+#SBATCH --requeue
+
+set -euo pipefail
+
+export VENV=../venv
+
+COMMAND=(
+  lunar_lander.py
+  "$@"
+)
+
+if [ -n "${SLURM_JOB_ID-}" ]; then COMMAND+=(--format npy --output "slurm-$SLURM_JOB_ID-model.npy"); fi
+
+../run.sh "${COMMAND[@]}"

cca086ab497cd190c90838ab0f50481e62679748
Filip Bartek <filip.bartek@hotmail.com>
2020-01-03 14:01:14 +0100

    Document some useful commands in run.sh

--- labs/run.sh
+++ labs/run.sh
@@ -6,6 +6,13 @@
 #SBATCH --time=60
 #SBATCH --requeue
 
+# ssh -L 6007:localhost:6007 cluster
+
+# srun --mem=1G --time=60 --partition=gpu --gres=gpu:1 --pty bash -i
+
+# VENV=../venv sbatch --time=240 --cpus-per-task=16 --job-name=walker-base ../run.sh walker.py
+# VENV=../venv sbatch --time=240 --cpus-per-task=16 --partition=gpu --gres=gpu:1 --job-name=walker-base ../run.sh walker.py
+
 set -euo pipefail
 
 # https://stackoverflow.com/a/949391/4054250
